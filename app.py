4429899dc510dabc522cf22a1be34765dca60af5
IdoKedem <idokedem12@gmail.com>
2024-09-06 15:51:35 +0300

    Compare texts feature

--- app.py
+++ app.py
@@ -3,16 +3,25 @@ from groq import Groq
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    if request.method == 'GET':
-        return render_template('index.html')
-
-    # METHOD = POST
+def get_groq_client():
     with open('api key.txt') as f:
         api_key = f.read()
-    groq_client = Groq(api_key=api_key)
+    return Groq(api_key=api_key)
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/analyze-text', methods=['GET', 'POST'])
+def analyze_text():
+    if request.method == 'GET':
+        return render_template('analyze_text.html')
+
+    # METHOD = POST
+
+    message = request.form['text']
+
+    groq_client = get_groq_client()
     instructions: str = \
         'You are a sentiment analysis assistant.' \
         'You reply with an in-depth sentiment analysis and ' \
@@ -20,7 +29,6 @@ def main():
         'You recognize contrasting sentiments in the same text and ' \
         'explain the dynamics between them.' \
         'Lastly, you add a summary of your analysis in a new line.'
-    message = request.form['text']
 
     groq_response = \
         groq_client.chat.completions.create(
@@ -38,10 +46,43 @@ def main():
         )
     sentiment = groq_response.choices[0].message.content
 
-    return render_template('index.html',
+    return render_template('analyze_text.html',
                            sentiment=sentiment, message=message)
 
 
+@app.route('/compare-texts', methods=['GET', 'POST'])
+def compare_texts():
+    if request.method == 'GET':
+        return render_template('compare_texts.html')
+    text1 = request.form['text1']
+    text2 = request.form['text2']
+
+    groq_client = get_groq_client()
+    instructions: str = \
+        'You are a sentiment analysis assistant.' \
+        'You receive two texts, the first text and the second text and analyze their sentiments.' \
+        'After doing so, you reply with a comparison between the sentiments of the two texts.' \
+        'In your comparison, refer to the meaning of the text, as well as ' \
+        'capital letters and punctuation marks, as they often ' \
+        'change the sentiment of a text.' \
+        'For example, the text "YES!" shows more excitement than a simple "yes", ' \
+        'Lastly, you summarize your comparison to a few lines at the end of your reply'
+    groq_response = \
+        groq_client.chat.completions.create(
+            messages=[
+                {
+                    'role': 'system',
+                    'content': instructions
+                },
+                {
+                    'role': 'user',
+                    'content': f'the first text: {text1}, the second text: {text2}'
+                }
+            ],
+            model='llama3-8b-8192'
+        )
+    comparison = groq_response.choices[0].message.content
+    return render_template('compare_texts.html', comparison=comparison)
 
 if __name__ == '__main__':
     app.run(debug=True)
